38ff87c342e09f67bca102592b45e38bab519c3c
Reinholds Zviedris <reinholds@zviedris.lv>
2022-10-29 20:45:54 +0300

    added run configs, updated main with mongo connection info

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 # Press ⌃R to execute it or replace it with your code.
-# Import library for fetching Elspot data
+import os
+import json
 from nordpool import elspot, elbas
 from pprint import pprint
 from datetime import date, datetime, timedelta
-import json
 from pymongo import MongoClient
 
 
@@ -24,7 +24,7 @@ def get_tomorrow(area_in, end_date):
 
 
 def get_today(area_in, end_date):
-    # Initialize class for fetching Elsbas prices
+    # Initialize class for fetching Elbas prices
     prices_bas = elbas.Prices()
 
     # Fetch hourly Elbas prices for area and print the resulting dictionary
@@ -38,8 +38,15 @@ def print_prices(today_data, tomorrow_data):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    area = 'LV'
+    area = os.getenv('SCRAPE_AREA', 'LV')
+    mongo_server = os.getenv('MONGO_SERVER', 'mongo')
+    mongo_port = os.getenv('MONGO_PORT', '27017')
+    mongo_database_name = os.getenv('MONGO_DATABASE', 'nordpool')
     now = datetime.now().time()
+
+    mongo_client = MongoClient(f"mongodb://{mongo_server}:{mongo_port}/")
+    mongo_db = mongo_client[mongo_database_name]
+
     if now.hour > 14 and now.minute > 59:
         today = date.today()
         tomorrow = today + timedelta(days=1)
